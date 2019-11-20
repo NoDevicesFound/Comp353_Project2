4b51ff5b90b36cf0213554b1831d18587611b891
Natalia Whiteley <nataliawhiteley11@gmail.com>
2019-11-20 12:27:30 -0500

    Added db model class and started login validation.

---
+++ dbModel.php
@@ -0,0 +1,49 @@
+<?php
+
+  class Model
+  {
+    $conn;
+    $servername = "frc353_2@encs.concordia.ca";
+    $username = "frc353_2";
+    $password = "AQfDNA";
+
+    function connectDB()
+    {
+      $conn = new mysqli($servername, $username, $password);
+      // TODO add error in case not connected
+    }
+
+    function disconnectDB()
+    {
+      $conn->close();
+    }
+
+    function findData($query)
+    {
+      // make array for results and connect to db
+      $data = array();
+      $this->connectDB();
+      // make query
+      $result = $this->$conn->query($query);
+
+      // store each row in an arry
+      while($row = $result->fetch_object())
+      {
+        $data[] = $row;
+      }
+
+      $this->disconnectDB();
+      return $data;
+    }
+
+    function updateData($query)
+    {
+
+    }
+
+    function addData($query)
+    {
+
+    }
+  }
+?>
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+	$uname = $_REQUEST['uname'].value;
+	$pwd = $_REQUEST['pwd'].value;
+
+	function validate() {
+		//make query to db to check uname and $pwd
+		// load user page if successful, else return error message
+	}
+?>
